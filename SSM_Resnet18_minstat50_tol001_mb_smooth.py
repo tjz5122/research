4415bbd861b5d369f9fe18ac336d41d1cf018f1d
tjz5122 <54636990+tjz5122@users.noreply.github.com>
2021-02-04 15:14:24 -0500

    Update and rename SSM_Resnet18_minstat50_tol0.01_mb_smooth.py to SSM_Resnet18_minstat50_tol001_mb_smooth.py

--- SSM_Resnet18_minstat50_tol001_mb_smooth.py
+++ SSM_Resnet18_minstat50_tol001_mb_smooth.py
@@ -422,7 +422,7 @@ for epoch in range(num_epochs):
 end = timer()
 
 
-file = open("SSM_Resnet18_minstat50_tol0.01_mb_smooth.txt","x")
+file = open("SSM_Resnet18_minstat50_tol001_mb_smooth.txt","x")
 file.write("train_accuracy_list = {}\n".format(str(train_accuracy_list)))
 file.write("test_accuracy_list = {}\n".format(str(test_accuracy_list)))
 file.write("lr_list = {}\n".format(str(lr_list)))
