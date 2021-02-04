5418a3ca5296b11e41bddb3f2db22a0b8f373219
tjz5122 <54636990+tjz5122@users.noreply.github.com>
2021-02-04 15:09:05 -0500

    Update and rename SSM_Resnet18_minstat100_tol0.005_mb_smooth.py to SSM_Resnet18_minstat100_tol0005_mb_smooth.py

--- SSM_Resnet18_minstat100_tol0005_mb_smooth.py
+++ SSM_Resnet18_minstat100_tol0005_mb_smooth.py
@@ -364,6 +364,7 @@ lr_list = []
 statistic_list = []
 avg_loss_list = []
 
+start = timer()
 for epoch in range(num_epochs):
     running_loss = 0
     my_model.train()
@@ -417,9 +418,11 @@ for epoch in range(num_epochs):
     statistic_list.append(optimizer.state['statistic'])
     current_lr = optimizer.state['lr']
     lr_list.append(current_lr)
+    
+end = timer()
+print("total computational time is", end - start)
 
-
-file = open("SSM_Resnet18_minstat100_tol0.005_mb_smooth.txt","x")
+file = open("SSM_Resnet18_minstat100_tol0005_mb_smooth.txt","x")
 file.write("train_accuracy_list = {}\n".format(str(train_accuracy_list)))
 file.write("test_accuracy_list = {}\n".format(str(test_accuracy_list)))
 file.write("lr_list = {}\n".format(str(lr_list)))
