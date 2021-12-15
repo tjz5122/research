dee18c648e29fb95e70b3a572085fc19b3ca7217
tjz5122 <54636990+tjz5122@users.noreply.github.com>
2021-12-14 19:19:48 -0500

    Update SSM_train.py

--- rrr/SSM_train.py
+++ rrr/SSM_train.py
@@ -330,13 +330,7 @@ class SSM(SSM_Optimizer):
     
     
     
-    
-
-### Design the Mgnet Network
-use_cuda = torch.cuda.is_available()
-print('Use GPU?', use_cuda)
-
-##MgNet
+###Mgnet
 class MgIte(nn.Module): 
     def __init__(self, A, S):
         super().__init__()
@@ -419,9 +413,7 @@ class MgNet(nn.Module):
         u = self.fc(u)
         return u
     
-    
-    
-##ResNet18
+###Resnet
 class BasicBlock(nn.Module):
     def __init__(self, in_planes, planes, stride):
         super().__init__()
@@ -473,10 +465,111 @@ class ResNet(nn.Module):
         out = F.avg_pool2d(out, 4)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
-        return out  
+        return out    
     
     
+    
+###pre-act resnet
+class PreActBlock(nn.Module):
+    '''Pre-activation version of the BasicBlock.'''
+    expansion = 1
 
+    def __init__(self, in_planes, planes, stride=1):
+        super(PreActBlock, self).__init__()
+        self.bn1 = nn.BatchNorm2d(in_planes)
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(x))
+        shortcut = self.shortcut(out) if hasattr(self, 'shortcut') else x
+        out = self.conv1(out)
+        out = self.conv2(F.relu(self.bn2(out)))
+        out += shortcut
+        return out
+
+
+class PreActBottleneck(nn.Module):
+    '''Pre-activation version of the original Bottleneck module.'''
+    expansion = 4
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(PreActBottleneck, self).__init__()
+        self.bn1 = nn.BatchNorm2d(in_planes)
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, self.expansion*planes, kernel_size=1, bias=False)
+
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(x))
+        shortcut = self.shortcut(out) if hasattr(self, 'shortcut') else x
+        out = self.conv1(out)
+        out = self.conv2(F.relu(self.bn2(out)))
+        out = self.conv3(F.relu(self.bn3(out)))
+        out += shortcut
+        return out
+
+
+class PreActResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(PreActResNet, self).__init__()
+        self.in_planes = 64
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.linear = nn.Linear(512*block.expansion, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+
+
+def PreActResNet18():
+    return PreActResNet(PreActBlock, [2,2,2,2])
+
+def PreActResNet34():
+    return PreActResNet(PreActBlock, [3,4,6,3])
+
+def PreActResNet50():
+    return PreActResNet(PreActBottleneck, [3,4,6,3])
+
+def PreActResNet101():
+    return PreActResNet(PreActBottleneck, [3,4,23,3])
+
+def PreActResNet152():
+    return PreActResNet(PreActBottleneck, [3,8,36,3])
+
+    
 
 
 ###args
@@ -496,13 +589,15 @@ def get_args():
     # For methods    
     parser.add_argument('--epochs', type=int, help='epoch number', default=120)
     
-    parser.add_argument('-b', '--batch-size', default=128, type=int, metavar='N', help='mini_batch size (default: 128)')
+    parser.add_argument('--batchsize', default=128, type=int, metavar='N', help='mini_batch size (default: 128)')
     
     parser.add_argument('--lr', '--learning-rate', default=1.0, type=float, metavar='LR', help='initial learning rate')
     
     parser.add_argument('--drop', default = 10, type=int, help='learning rate drop factor')
     
-    parser.add_argument('-m', '--momentum', default=0.8, type=float, metavar='M', help='momentum')
+    parser.add_argument('--momentum', default=0.9, type=float, metavar='M', help='momentum')
+    
+    parser.add_argument('--dampening', default=0.9, type=float, metavar='D', help='dampening')
     
     parser.add_argument('--weight-decay', '--wd', default=5e-4, type=float, metavar='W', help='weight decay (e.g. 5e-4)')
             
@@ -525,9 +620,7 @@ def get_args():
     parser.add_argument('--data', type=str, help='cifar10, cifar100 or mnist', default='cifar10')
     
     # For models    
-    parser.add_argument('--ch', metavar='channel_number', default=128, type=int, help='channel number (default: 256)')
-    
-    parser.add_argument('--iter', metavar='iter', default='2222')
+    parser.add_argument('--model', type=str, help='mgnet128, resnet34 or preresnet18', default='mgnet128')
     
     return parser.parse_args()
 
@@ -536,22 +629,37 @@ def main():
     args = get_args()  # get the arguments
 
     #implementation
-    minibatch_size = args.batch_size
+    minibatch_size = args.batchsize
     num_epochs =  args.epochs
-    degree = args.ch
-    if args.data == 'cifar10' or 'cifar100':
-        num_channel_input = 3 # since cifar10
-    num_channel_u = degree # usaually take channel u and f same, suggested value = 64,128,256,512.....
-    num_channel_f = degree
     if args.data == 'cifar10':
         num_classes = 10
-    if args.data == 'cifar100':
+        num_channel_input = 3 
+    elif args.data == 'cifar100':
         num_classes = 100
-    num_iteration = [int(i) for i in list(args.iter)] # for each layer do 1 iteration or you can change to [2,2,2,2] or [2,1,1,1]
+        num_channel_input = 3 
+
     
     # Step 1: Define a model
-    my_model = MgNet(num_channel_input, num_iteration, num_channel_u, num_channel_f, num_classes)
+    if args.model == 'mgnet128':
+        my_model = MgNet(num_channel_input, [2,2,2,2], 128, 128, num_classes)
+    elif args.model  == 'mgnet256':
+        my_model = MgNet(num_channel_input, [2,2,2,2], 256, 256, num_classes)
+    elif args.model  == 'resnet18':
+        my_model = ResNet(BasicBlock, [2,2,2,2], num_classes=num_classes)
+    elif args.model  == 'resnet34':
+        my_model = ResNet(BasicBlock, [3,4,6,3], num_classes=num_classes)
+    elif args.model  == 'preactresnet18':
+        my_model = PreActResNet18()
+    elif args.model  == 'preactresnet34':
+        my_model = PreActResNet34()
+    elif args.model  == 'densenet121':
+        my_model = models.densenet121()
+    elif args.model  == 'densenet161':
+        my_model = models.densenet161()
+    elif args.model  == 'efficientnet':
+        my_model = EfficientNet.from_pretrained('efficientnet-b0')
         
+
     if use_cuda:
         my_model = my_model.cuda()
     
@@ -563,41 +671,32 @@ def main():
     if args.data == 'cifar10':
         normalize = torchvision.transforms.Normalize(mean=(0.4914, 0.4822, 0.4465), std=(0.2023, 0.1994, 0.2010))
     
-        transform_train = torchvision.transforms.Compose([torchvision.transforms.RandomCrop(32, padding=4),
-                                                          torchvision.transforms.RandomHorizontalFlip(),
-                                                          torchvision.transforms.ToTensor(),
-                                                          normalize])
-        transform_test  = torchvision.transforms.Compose([torchvision.transforms.ToTensor(),normalize])
-        trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
-        trainloader = torch.utils.data.DataLoader(trainset, batch_size=minibatch_size, shuffle=True)
-        testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
-        testloader = torch.utils.data.DataLoader(testset, batch_size=minibatch_size, shuffle=False)
-    
     if args.data == 'cifar100':
         normalize = torchvision.transforms.Normalize(mean=(0.5071, 0.4867, 0.4408), std=(0.2675, 0.2565, 0.2761))
     
-        transform_train = torchvision.transforms.Compose([torchvision.transforms.RandomCrop(32, padding=4),
-                                                          torchvision.transforms.RandomHorizontalFlip(),
-                                                          torchvision.transforms.ToTensor(),
-                                                          normalize])
-        transform_test  = torchvision.transforms.Compose([torchvision.transforms.ToTensor(),normalize])
-        trainset = torchvision.datasets.CIFAR100(root='./data', train=True, download=True, transform=transform_train)
-        trainloader = torch.utils.data.DataLoader(trainset, batch_size=minibatch_size, shuffle=True)
-        testset = torchvision.datasets.CIFAR100(root='./data', train=False, download=True, transform=transform_test)
-        testloader = torch.utils.data.DataLoader(testset, batch_size=minibatch_size, shuffle=False)
+    transform_train = torchvision.transforms.Compose([torchvision.transforms.RandomCrop(32, padding=4),
+                                                      torchvision.transforms.RandomHorizontalFlip(),
+                                                      torchvision.transforms.ToTensor(),
+                                                      normalize])
+    transform_test  = torchvision.transforms.Compose([torchvision.transforms.ToTensor(),normalize])
+    trainset = torchvision.datasets.CIFAR100(root='./data', train=True, download=True, transform=transform_train)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=minibatch_size, shuffle=True)
+    testset = torchvision.datasets.CIFAR100(root='./data', train=False, download=True, transform=transform_test)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=minibatch_size, shuffle=False)
     
+    optimizer = SSM(my_model.parameters(), lr=args.lr, weight_decay=args.weight_decay, momentum=args.momentum, dampening=args.dampening, testfreq=len(trainloader), var_mode=args.varmode, leak_ratio=args.lk, minN_stats=args.minstat, mode=args.keymode, samplefreq=args.samplefreq, significance=args.sig, drop_factor=args.drop, trun=args.trun)
     
-    optimizer = SSM(my_model.parameters(), lr=args.lr, weight_decay=args.weight_decay, momentum=args.momentum, testfreq=len(trainloader), var_mode=args.varmode, leak_ratio=args.lk, minN_stats=args.minstat, mode=args.keymode, samplefreq=args.samplefreq, significance=args.sig, drop_factor=args.drop, trun=args.trun)
-    
-    train_accuracy_list = []
     test_accuracy_list = []
+    lr_list = []
+    statistic_list = []
     avg_loss_list = []
-    decrease_record = []
+    time_list = []
     
-    start = timer()
+    
     #Step 4: Train the NNs
     # One epoch is when an entire dataset is passed through the neural network only once.
     for epoch in range(num_epochs):
+        start = timer()
         running_loss = 0
         my_model.train()
         for i, (images, labels) in enumerate(trainloader):
@@ -606,7 +705,10 @@ def main():
               labels = labels.cuda()
     
             # Forward pass to get the loss
-            outputs = my_model(0,images)   # We need additional 0 input for u in MgNet
+            if (args.model == "mgnet128") or (args.model == "mgnet256"):
+                outputs = my_model(0,images)   # We need additional 0 input for u in MgNet
+            else:
+                outputs = my_model(images) 
             loss = criterion(outputs, labels)
             optimizer.state['loss'] = loss.item()
             # Backward and compute the gradient
@@ -617,23 +719,8 @@ def main():
         avg_loss_list.append(running_loss)
         
         
-      # Training accuracy
-        my_model.eval()
-        correct = 0
-        total = 0
-        for i, (images, labels) in enumerate(trainloader):
-            with torch.no_grad():
-              if use_cuda:
-                  images = images.cuda()
-                  labels = labels.cuda()  
-              outputs = my_model(0,images)   # We need additional 0 input for u in MgNet
-              p_max, predicted = torch.max(outputs, 1) 
-              total += labels.size(0)
-              correct += (predicted == labels).sum()
-        training_accuracy = float(correct)/total
-        train_accuracy_list.append(training_accuracy)     
-        
         # Test accuracy
+        my_model.eval()
         correct = 0
         total = 0
         for i, (images, labels) in enumerate(testloader):
@@ -641,79 +728,42 @@ def main():
               if use_cuda:
                   images = images.cuda()
                   labels = labels.cuda()
-              outputs = my_model(0,images)   # We need additional 0 input for u in MgNet
+              if (args.model == "mgnet128") or (args.model == "mgnet256"):
+                  outputs = my_model(0,images)   # We need additional 0 input for u in MgNet
+              else:
+                  outputs = my_model(images) 
               p_max, predicted = torch.max(outputs, 1) 
               total += labels.size(0)
               correct += (predicted == labels).sum()
         test_accuracy = float(correct)/total
+        end = timer()
+        
+        
         test_accuracy_list.append(test_accuracy)
-        current_lr = optimizer.state['lr']
-        if optimizer.state['stats_stationary'] == 1:
-            decrease_record.append((epoch,test_accuracy,running_loss))
+        statistic_list.append(optimizer.state['statistic'])
+        lr_list.append(optimizer.state['lr'])
+        time_list.append(end - start)
             
-        
-    end = timer()
-    decrease_time = np.log10(args.lr/current_lr) / np.log10(10/args.drop) 
-
     
-    print("complete")
+    if args.trun == 0.02:
+        sign_trun = '002'
+    if args.trun == 0.03:
+        sign_trun = '003'
+    loglr_list = list(np.log10(np.array(lr_list)))
     
-    if len(decrease_record) == 0:
-        e1,a1,l1,e2,a2,l2,e3,a3,l3,e4,a4,l4= [0,0,0,0,0,0,0,0,0,0,0,0]; 
-    elif len(decrease_record) == 1:
-        e1,a1,l1 = decrease_record[0]; e2,a2,l2,e3,a3,l3,e4,a4,l4 = [0,0,0,0,0,0,0,0,0]
-    elif len(decrease_record) == 2:
-        e1,a1,l1 = decrease_record[0]; e2,a2,l2 = decrease_record[1]; e3,a3,l3,e4,a4,l4 = [0,0,0,0,0,0]
-    elif len(decrease_record) == 3:
-        e1,a1,l1 = decrease_record[0]; e2,a2,l2 = decrease_record[1]; e3,a3,l3 = decrease_record[2]; e4,a4,l4 = [0,0,0]
-    else:
-        e1,a1,l1 = decrease_record[0]; e2,a2,l2 = decrease_record[1]; e3,a3,l3 = decrease_record[2]; e4,a4,l4 = decrease_record[3]
-        
-
-    
-    final_train_accuracy = train_accuracy_list[-1]
-    final_test_accuracy = test_accuracy_list[-1]
-    final_avg_loss = avg_loss_list[-1]
-    total_time = end - start
+    print('complete')
     # example of files for training  
-    f = open("SSM_Mgnet_data", 'a')
-    f.write('{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}\n'.format(
-                             args.count,
-                             args.ch,
-                             args.iter,
-                             args.data,
-                             args.epochs,
-                             args.lr,
-                             args.weight_decay,
-                             args.momentum,
-                             args.batch_size,
-                             args.trun,
-                             args.drop,
-                             args.sig,
-                             args.lk,
-                             args.minstat,
-                             args.samplefreq,
-                             args.varmode,
-                             args.keymode,
-                             args.trail,
-                             decrease_time,
-                             e1,
-                             a1,
-                             l1,
-                             e2,
-                             a2,
-                             l2,
-                             e3,
-                             a3,
-                             l3,
-                             e4,
-                             a4,
-                             l4,
-                             final_train_accuracy,
-                             final_test_accuracy,
-                             final_avg_loss,
-                             total_time))
+    f = open("SSM_train_data", 'a')
+    f.write('{}_trun{}_lk{}_sf{}_d{}_testaccu = {}\n'.format(args.model, sign_trun, args.lk, args.samplefreq, args.dampening, test_accuracy_list))
+    f.write('{}_trun{}_lk{}_sf{}_d{}_loglr = {}\n'.format(args.model, sign_trun, args.lk, args.samplefreq, args.dampening, loglr_list))
+    f.write('{}_trun{}_lk{}_sf{}_d{}_stat = {}\n'.format(args.model, sign_trun, args.lk, args.samplefreq, args.dampening, statistic_list))
+    f.write('{}_trun{}_lk{}_sf{}_d{}_loss = {}\n'.format(args.model, sign_trun, args.lk, args.samplefreq, args.dampening, avg_loss_list))
+    f.write('{}_trun{}_lk{}_sf{}_d{}_time = {}\n'.format(args.model, sign_trun, args.lk, args.samplefreq, args.dampening, time_list))
+    f.write("\n")
 
     f.close()
 
 main()
+
+
+
