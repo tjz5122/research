f34f3a9ceb6965f7be383a53801a479939f7dd6a
tjz5122 <54636990+tjz5122@users.noreply.github.com>
2021-12-14 19:25:32 -0500

    Update SSM_train.py

--- rrr/SSM_train.py
+++ rrr/SSM_train.py
@@ -329,7 +329,142 @@ class SSM(SSM_Optimizer):
         return None
     
 
+###Mgnet
+class MgIte(nn.Module): 
+    def __init__(self, A, S):
+        super().__init__()
+        
+        self.A = A
+        self.S = S
 
+        self.bn1 =nn.BatchNorm2d(A.weight.size(0)) 
+        self.bn2 =nn.BatchNorm2d(S.weight.size(0)) 
+    
+    def forward(self, out):
+        u, f = out 
+        u = u + F.relu(self.bn2(self.S(F.relu(self.bn1((f-self.A(u))))))) 
+        out = (u, f)
+        return out
+
+
+
+class MgRestriction(nn.Module):
+    def __init__(self, A_old, A, Pi, R):
+        super().__init__()
+
+        self.A_old = A_old
+        self.A = A
+        self.Pi = Pi
+        self.R = R
+
+        self.bn1 = nn.BatchNorm2d(Pi.weight.size(0))   
+        self.bn2 = nn.BatchNorm2d(R.weight.size(0))    
+
+    def forward(self, out):
+        u_old, f_old = out 
+        u = F.relu(self.bn1(self.Pi(u_old)))                              
+        f = F.relu(self.bn2(self.R(f_old-self.A_old(u_old)))) + self.A(u)        
+        out = (u,f)
+        return out
+
+
+class MgNet(nn.Module):
+    def __init__(self, num_channel_input, num_iteration, num_channel_u, num_channel_f, num_classes):
+        super().__init__()
+        self.num_iteration = num_iteration
+        self.num_channel_u = num_channel_u
+        self.conv1 = nn.Conv2d(num_channel_input, num_channel_f, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(num_channel_f)        
+
+        
+        A = nn.Conv2d(num_channel_u, num_channel_f, kernel_size=3, stride=1, padding=1, bias=False)
+        S = nn.Conv2d(num_channel_f, num_channel_u, kernel_size=3,stride=1, padding=1, bias=False)
+        layers = []
+        for l, num_iteration_l in enumerate(num_iteration):
+            for i in range(num_iteration_l):
+                layers.append(MgIte(A, S)) 
+            setattr(self, 'layer'+str(l), nn.Sequential(*layers))
+
+            if l < len(num_iteration)-1:
+                A_old = A 
+                A = nn.Conv2d(num_channel_u, num_channel_f, kernel_size=3,stride=1, padding=1, bias=False)
+                S = nn.Conv2d(num_channel_f, num_channel_u, kernel_size=3,stride=1, padding=1, bias=False)
+                Pi = nn.Conv2d(num_channel_u, num_channel_u, kernel_size=3,stride=2, padding=1, bias=False)
+                R = nn.Conv2d(num_channel_f, num_channel_f, kernel_size=3, stride=2, padding=1, bias=False)
+                layers= [MgRestriction(A_old, A, Pi, R)] 
+        
+        self.pooling = nn.AdaptiveAvgPool2d(1) 
+        self.fc = nn.Linear(num_channel_u ,num_classes) 
+
+    def forward(self, u, f):
+        f = F.relu(self.bn1(self.conv1(f)))                
+        if use_cuda:                                        
+            u = torch.zeros(f.size(0),self.num_channel_u,f.size(2),f.size(3), device=torch.device('cuda')) 
+        else:
+            u = torch.zeros(f.size(0),self.num_channel_u,f.size(2),f.size(3))        
+        out = (u, f) 
+
+        for l in range(len(self.num_iteration)):
+            out = getattr(self, 'layer'+str(l))(out)
+        u, f = out       
+        u = self.pooling(u) #do avg pooling
+        u = u.view(u.shape[0], -1)  #reshape u batch_Size to vector
+        u = self.fc(u)
+        return u
+    
+###Resnet
+class BasicBlock(nn.Module):
+    def __init__(self, in_planes, planes, stride):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1:
+            self.shortcut = nn.Sequential(nn.Conv2d(in_planes, planes, kernel_size=1, stride=stride, bias=False),
+                                          nn.BatchNorm2d(planes))
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super().__init__()
+        self.in_planes = 64
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.linear = nn.Linear(512, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out    
 
     
 
